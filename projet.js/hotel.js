f9831006e732f90ccc6e0345b5a4f81892148e05
Luiza021 <luizacerqueiraa@hotmail.comm>
2021-06-10 11:52:47 -0300

    projet Hotel

---
+++ projet.js/hotel.js
@@ -0,0 +1,29 @@
+var hotel = {
+	nome: 'Quay',
+	quartos: 40,
+	reservados: 25,
+	academia: true,
+	checaDisponibilidade: function (){
+	return this.quartos - this.reservados;
+  }
+};
+var subResultado = document.getElementById('quartos'); 
+subResultado.textContent += hotel.quartos;
+
+
+
+function btnReserva(){
+    hotel.reservados--;
+    mostrar(hotel.reservados);
+}
+
+
+function mostrar(valor) {
+
+    if(hotel.reservados > 0){
+        document.getElementById("vagas").innerHTML = valor;
+    }
+    else{
+        document.getElementById("vagas").innerHTML = 'Não há vagas';
+    }
+}
