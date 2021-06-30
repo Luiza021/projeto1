a00fb3a544c789806cf49baf6769a6eaa7989f81
Luiza021 <luizacerqueiraa@hotmail.comm>
2021-06-30 09:03:10 -0300

    26

--- PHP01/model/Conecta.php
+++ PHP01/model/Conecta.php
@@ -1,18 +1,19 @@
 <?php
 class Conecta {
-    private stactic $url  = "localhost:3306";
-    private stactic $user = "root";
-    private  stactic $passaword = "senac";
-    private stactic $base = "dbphp01";
+    private static $url  = "localhost:3306";
+    private static $user = "root";
+    private  static $password = "senac";
+    private static $base = "dbphp01";
     public $db;
 
-    public function(){
+    public function Luiza(){
         $db = $this->conectadb;
     }
   
     public function conectadb(){
-        return mysqli_connect($this->getUrl(),$this->getPassaword(),
-        $this->getBase()) or die (mysqli_errno($link)); 
+        $db=  mysqli_connect($this->getUrl(),$this->getPassword(),
+        $this->getBase()) or die (mysqli_errno($db)); 
+        return $db;
     }
 
     function getUrl(){
@@ -22,5 +23,28 @@ class Conecta {
     function getUser(){
         return $this->user;
     }
-    
+  
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getBase()
+    {
+        return $this->base;
+    }
+
+    public function setBase($base)
+    {
+        $this->base = $base;
+
+        return $this;
+    }
 }
--- PHP01/model/daoGenerico.php
+++ PHP01/model/daoGenerico.php
@@ -2,17 +2,17 @@
 include_once ('bd/Conecta.php');
 include_once '../model/Pessoa.php';
 
-class daoGenerico{
+class daoGenerico {
     public $conn;
-    $sql = "insert into pessoa valeus (null,'".$p->getName()."','".$p-> getdtNasc()."','".$p-> getlogin().
-    "','".$p-> getsenha()."','".$p-> getperfil()."','".$p-> getemail()."','".$p-> getcpf()."');
-    if
+
     function inserir (Pessoa $p){
         $conn = new Conecta();
-               if(mysqli_query($conn->conectadb(),$sql){
+        $sql = "insert into pessoa valeus (null,'".$p->getNome()."','".$p-> getdtNasc()."','".$p-> getlogin().
+        "','".$p-> getsenha()."','".$p-> getperfil()."','".$p-> getemail()."','".$p-> getcpf().")";
+               if(mysqli_query($conn->conectadb(),$sql))
         return "Dados cadastrados com sucesso!";
-        }else{ 
+        else 
         return "Erro na conexão com Dados!";
-        }
+        
     }
 }
