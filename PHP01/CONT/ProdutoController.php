be3761ed5a13363d4a7da479473d972e957df806
Luiza021 <luizacerqueiraa@hotmail.comm>
2021-07-07 11:26:25 -0300

    07/07

---
+++ PHP01/CONT/ProdutoController.php
@@ -0,0 +1,21 @@
+
+<?php
+include_once '\xampp\htdocs\projeto1\PHP01\Controller'
+include_once '\xampp\htdocs\projeto1\PHP01\Controller'
+
+class ProdutoController{
+      public function inserirProduto($nomeProduto,$vlrcompra,$vlrvenda,$qtdEstoque){
+      $produto = new Produto();
+      $produto->setNomeProduto($nomeProduto);  
+      $produto->setVlrCompra($nomeProduto);
+      $produto->setVlrVenda($nomeProduto);
+      $produto->setQtdEstoque($nomeProduto);
+
+      $daoProduto = new DaoProduto();
+      return $daoProduto->insert($produto);
+}
+  //metedo para carregar a lista de produtos que vem da DAO
+  public function listarProdutos(){
+      $daoProduto = new daoProduto();
+      return $daoProduto ->listarProdutoDAO();
+?>
